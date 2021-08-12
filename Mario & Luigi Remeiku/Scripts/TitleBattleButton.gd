52847e722de9bb8b7ffa0157bccb65b4ed4f7a92
CharlotteaDev <66517745+CharlotteaDev@users.noreply.github.com>
2021-08-12 17:42:14 +0200

    added godot stuff lol

---
+++ Mario & Luigi Remeiku/Scripts/TitleBattleButton.gd
@@ -0,0 +1,11 @@
+extends Button
+
+onready var animationPlayerRect : AnimationPlayer = get_node("../AnimationPlayerRect")
+onready var animationPlayer : AnimationPlayer = get_node("AnimationPlayer")
+var delayTimer = 1.5
+
+func _on_Button_pressed():
+	animationPlayerRect.play("Fade out")
+	animationPlayer.play("FadeOut")
+	yield(get_tree().create_timer(delayTimer), "timeout")
+	get_tree().change_scene("res://Scenes/Main.tscn")
