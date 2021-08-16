3de57434741809a1601afc80df326200d1c2864f
CharlotteaDev <yyoshidevv@gmail.com>
2021-08-17 01:41:21 +0200

    Added code because tabs (check HealthUI.script)

--- Mario & Luigi Remeiku/Scripts/HPScript.gd
+++ Mario & Luigi Remeiku/Scripts/HPScript.gd
@@ -1,10 +1,22 @@
 extends Sprite
 
-var totHP = null
+var totHP = 195
 
-onready var xxoHP = get_node("../XXOH")
-onready var xoxHP = get_node("../XOXH")
-onready var oxxHP = get_node("../OXXH")
+export var xxoHP_path : NodePath
+export var xoxHP_path : NodePath
+export var oxxHP_path : NodePath
+
+onready var xxoHP = get_node(xxoHP_path)
+onready var xoxHP = get_node(xoxHP_path)
+onready var oxxHP = get_node(oxxHP_path)
 
 func _ready(): 
+#
+#	xxoHP.set_animation("K")
+#	xoxHP.set_animation("K")
+#	oxxHP.set_animation("K")
+#
+#	xxoHP.set_frame(5)
+#	xoxHP.set_frame(9)
+#	oxxHP.set_frame(1)
 	pass
