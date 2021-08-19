b4a1fc555ecf9445344b65720dd4ec8ae0a5a910
YoshiDev <66517745+CharlotteaDev@users.noreply.github.com>
2021-08-19 23:02:07 +0200

    HPScript changes (currently adding debug)

--- Mario & Luigi Remeiku/Scripts/BPScript.gd
+++ Mario & Luigi Remeiku/Scripts/BPScript.gd
@@ -1,3 +1,37 @@
 extends Sprite
 
-var BP = 999
+var totSP = 14
+
+#HP numbers nodes
+onready var xxoSP = get_node("./001S")
+onready var xoxSP = get_node("./010S")
+onready var oxxSP = get_node("./100S")
+
+#HP numbers values/current frame
+onready var x1 = xxoSP.frame
+onready var x2 = xoxSP.frame
+onready var x3 = oxxSP.frame
+
+func _ready():
+
+	xxoSP.set_animation("R")
+	xoxSP.set_animation("R")
+	oxxSP.set_animation("R")
+
+	xxoSP.set_frame(4)
+	xoxSP.set_frame(1)
+	oxxSP.set_frame(0)
+
+	#For making numbers disappear when they've reached 0, but not removing them when they're at 10 for example.
+	if oxxSP.frame == 0:
+		oxxSP.visible = false
+	elif xoxSP.frame == 0:
+		xoxSP.visible = false
+	elif xxoSP.frame == 0:
+		xxoSP.visible = false
+
+	#For me to see the value for stuff is working
+	print(totSP)
+	print(x1)
+	print(x2)
+	print(x3)
--- Mario & Luigi Remeiku/Scripts/HPScript.gd
+++ Mario & Luigi Remeiku/Scripts/HPScript.gd
@@ -1,21 +1,54 @@
 extends Sprite
 
-var totHP = 195
+#HP numbers nodes
+onready var xxoHP = get_node("./001H")
+onready var xoxHP = get_node("./010H")
+onready var oxxHP = get_node("./100H")
 
-export var xxoHP_path : NodePath
-export var xoxHP_path : NodePath
-export var oxxHP_path : NodePath
+#HP numbers values/current frame
+onready var x1 = xxoHP.frame
+onready var x2 = xoxHP.frame
+onready var x3 = oxxHP.frame
 
-onready var xxoHP = get_node(xxoHP_path)
-onready var xoxHP = get_node(xoxHP_path)
-onready var oxxHP = get_node(oxxHP_path)
+var totHP = int()
 
-func _ready(): 
+func _ready():
 
-	xxoHP.set_animation("K")
-	xoxHP.set_animation("K")
-	oxxHP.set_animation("K")
+	xxoHP.set_animation("N")
+	xoxHP.set_animation("N")
+	oxxHP.set_animation("N")
 
 	xxoHP.set_frame(5)
-	xoxHP.set_frame(9)
-	oxxHP.set_frame(1)
+	xoxHP.set_frame(1)
+	oxxHP.set_frame(0)
+	
+# warning-ignore:unused_variable
+# warning-ignore:unused_variable
+	var totHP = int(str(x3) + str(x2) + str(x1))
+	
+	if Input.is_action_pressed("A"):
+		xxoHP.set_frame(x1 + 5)
+
+	#For making numbers disappear when they've reached 0, but not removing them when they're at 10 for example.
+	if oxxHP.frame == 0:
+		oxxHP.visible = false
+	elif xoxHP.frame == 0:
+		xoxHP.visible = false
+	elif xxoHP.frame == 0:
+		xxoHP.visible = false
+
+func _on_001H_frame_changed():
+
+	#For making numbers disappear when they've reached 0, but not removing them when they're at 10 for example.
+	if oxxHP.frame == 0:
+		oxxHP.visible = false
+	elif xoxHP.frame == 0:
+		xoxHP.visible = false
+	elif xxoHP.frame == 0:
+		xxoHP.visible = false
+
+	#For me to see the value for stuff is working
+	print(totHP)
+	print(x1)
+	print(x2)
+	print(x3)
