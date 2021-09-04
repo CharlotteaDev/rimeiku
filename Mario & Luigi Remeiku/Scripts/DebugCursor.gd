6c78f93d52854829d6603a8b4b96bff8980d7625
CharlotteaDev <yyoshidevv@gmail.com>
2021-09-04 18:29:25 +0200

    Started work on DebugCursor, not working atm

--- Mario & Luigi Remeiku/Scripts/DebugCursor.gd
+++ Mario & Luigi Remeiku/Scripts/DebugCursor.gd
@@ -1,7 +1,31 @@
 extends Sprite
 
-onready var pos = 1
+onready var posX = 1
+onready var posY = 1
 
-func _ready():
-	if Input.is_action_pressed("right"):
-		print("right")
+func _input(event):
+
+	if event.is_action_pressed("right"):
+		posX = posX+1
+		print("x",posX)
+		frame = 2
+	if posX == 3:
+		posX = posX-1
+
+	if event.is_action_pressed("left"):
+		posX = posX-1
+		print("x",posX)
+	if posX == -1:
+		posX = posX+1
+
+	if event.is_action_pressed("up"):
+		posY = posY-1
+		print("y",posY)
+	elif posY == 1:
+		posY = 0
+
+	if event.is_action_pressed("down"):
+		posY = posY+1
+		print("y",posY)
+	elif posY == 0:
+		posY = 1
