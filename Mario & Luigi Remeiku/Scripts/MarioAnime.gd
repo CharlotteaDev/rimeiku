e8406e0334afc54cc712acd4641415a0b53d622f
YoshiDev <66517745+CharlotteaDev@users.noreply.github.com>
2021-10-11 01:25:52 +0200

    fixed scaling and positioning on m&l

--- Mario & Luigi Remeiku/Scripts/MarioAnime.gd
+++ Mario & Luigi Remeiku/Scripts/MarioAnime.gd
@@ -1,11 +1,14 @@
 extends AnimationPlayer
 
 onready var ManimationPlayer : AnimationPlayer = get_node("../AnimationPlayer")
-onready var delayTimer = 1.2
+onready var delayTimer = 3.7
+onready var disabled = true
 
 func _ready():
 	yield(get_tree().create_timer(delayTimer), "timeout")
+	disabled = false
 
 func _input(event):
-	if event.is_action_pressed("A"):
+	if event.is_action_pressed("A") and !disabled:
 		ManimationPlayer.play("jump")
+		print("jumped")
