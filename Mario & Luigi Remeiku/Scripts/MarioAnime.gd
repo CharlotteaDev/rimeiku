418bccf2c165e2eaff2027b9071fc4bce578a364
YoshiDev <66517745+CharlotteaDev@users.noreply.github.com>
2021-10-14 00:17:27 +0200

    was adding jump and i'm getting node reference errors again so i give up for now

--- Mario & Luigi Remeiku/Scripts/MarioAnime.gd
+++ Mario & Luigi Remeiku/Scripts/MarioAnime.gd
@@ -1,14 +1,19 @@
 extends AnimationPlayer
 
 onready var ManimationPlayer : AnimationPlayer = get_node("../AnimationPlayer")
-onready var delayTimer = 3.7
+onready var Mario = get_node("./Mario")
+
+onready var delayTimerDisable = 3.7
+onready var delayTimerJumpcool = 0.5
 onready var disabled = true
+onready var stateMachine = "yourTurn"
 
 func _ready():
-	yield(get_tree().create_timer(delayTimer), "timeout")
+	yield(get_tree().create_timer(delayTimerDisable), "timeout")
 	disabled = false
+	Mario.frames
 
 func _input(event):
 	if event.is_action_pressed("A") and !disabled:
-		ManimationPlayer.play("jump")
+	
 		print("jumped")
