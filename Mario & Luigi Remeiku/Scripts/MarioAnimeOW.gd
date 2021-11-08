85b1366715ef8d332c03bc00becbec9a57a36498
YoshiDev <66517745+CharlotteaDev@users.noreply.github.com>
2021-11-08 16:02:06 +0100

    Added 2 other walking directions

--- Mario & Luigi Remeiku/Scripts/MarioAnimeOW.gd
+++ Mario & Luigi Remeiku/Scripts/MarioAnimeOW.gd
@@ -5,14 +5,16 @@ onready var stateMachine = "idle"
 onready var sprite : AnimatedSprite = $Mario
 
 func _ready():
-	sprite.speed_scale = 2
+	if stateMachine == "idle":
+		sprite.speed_scale = 2
+		sprite.play("idle")
+	if stateMachine == "movingS":
+		sprite.speed_scale = 6
+		sprite.play("moveSouth")
+
 
 func  _input(event):
-	if Input.is_action_pressed("down"):
-		sprite.play("moveSouth")
-		sprite.speed_scale = 6
-		print("moving south")
-	else:
-		sprite.play("idle")
-		sprite.speed_scale = 2
-		print("idle")
+	if Input.is_action_pressed("down") and !Input.is_action_pressed("left"):
+		facingDirection = "south"
+		stateMachine = "movingS"
+		position.x + 1
