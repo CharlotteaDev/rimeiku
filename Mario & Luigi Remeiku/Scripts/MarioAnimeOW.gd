939caa92de03bf7845f1f0f3a0415bc302be9a02
YoshiDev <66517745+CharlotteaDev@users.noreply.github.com>
2021-11-10 00:42:58 +0100

    Finished movement code

--- Mario & Luigi Remeiku/Scripts/MarioAnimeOW.gd
+++ Mario & Luigi Remeiku/Scripts/MarioAnimeOW.gd
@@ -1,20 +1,66 @@
 extends KinematicBody2D
 
-onready var facingDirection = "south"
-onready var stateMachine = "idle"
+onready var facingDirection = ""
+onready var stateMachine = ""
 onready var sprite : AnimatedSprite = $Mario
 
+export var speed = 300
+var direction = Vector2()
+
 func _ready():
-	if stateMachine == "idle":
-		sprite.speed_scale = 2
-		sprite.play("idle")
-	if stateMachine == "movingS":
-		sprite.speed_scale = 6
-		sprite.play("moveSouth")
+	sprite.speed_scale = 3
 
+func  get_input():
+	direction = Vector2()
 
-func  _input(event):
-	if Input.is_action_pressed("down") and !Input.is_action_pressed("left"):
-		facingDirection = "south"
-		stateMachine = "movingS"
-		position.x + 1
+	if Input.is_action_pressed("right"):
+		direction.x += 1
+	if Input.is_action_pressed("left"):
+		direction.x -= 1 
+	if Input.is_action_pressed("down"):
+		direction.y += 1 
+	if Input.is_action_pressed("up"):
+		direction.y -= 1 
+
+	match direction:
+		Vector2(-1,-1):
+			sprite.animation = "moveWestNorth"
+			facingDirection = "WestNorth"
+			sprite.speed_scale = 6
+		Vector2(-1,0):
+			sprite.animation = "moveWest"
+			facingDirection = "West"
+			sprite.speed_scale = 6
+		Vector2(-1,1):
+			sprite.animation = "moveSouthWest"
+			facingDirection = "SouthWest"
+			sprite.speed_scale = 6
+		Vector2(1,-1):
+			sprite.animation = "moveNorthEast"
+			facingDirection = "NorthEast"
+			sprite.speed_scale = 6
+		Vector2(1,0):
+			sprite.animation = "moveEast"
+			facingDirection = "East"
+			sprite.speed_scale = 6
+		Vector2(1,1):
+			sprite.animation = "moveEastSouth"
+			facingDirection = "EastSouth"
+			sprite.speed_scale = 6
+		Vector2(0,-1):
+			sprite.animation = "moveNorth"
+			facingDirection = "North"
+			sprite.speed_scale = 6
+		Vector2(0,1):
+			sprite.animation = "moveSouth"
+			facingDirection = "South"
+			sprite.speed_scale = 6
+		Vector2(0,0):
+			sprite.animation = ("idle"+str(facingDirection))
+			sprite.speed_scale = 3
+
+	direction = direction.normalized() * speed
+
+func _physics_process(_delta):
+	get_input()
+	direction = move_and_slide(direction)
