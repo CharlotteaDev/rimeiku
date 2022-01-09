57d2d9b486ad0c747498f319435dbe187920d23b
CharlotteaDev <yyoshidevv@gmail.com>
2022-01-09 15:31:08 +0100

    test commit

---
+++ Mario & Luigi Remeiku/scenes/o_cam.gd
@@ -0,0 +1,13 @@
+extends Camera2D
+
+func _process(delta):
+	if GlobalSingleton.current_area == "DebugRoom":
+		limit_left = -413
+		limit_right = 1023
+		limit_top = -712
+		limit_bottom = 415
+	if GlobalSingleton.current_area == "StardustFields1":
+		limit_left = -413
+		limit_right = 1023
+		limit_top = -429
+		limit_bottom = 415
