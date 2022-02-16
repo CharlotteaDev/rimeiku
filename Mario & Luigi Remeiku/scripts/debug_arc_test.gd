2b82b2ec54df17a272df73e096b7849abc0c76ca
CharlotteaDev <yyoshidevv@gmail.com>
2022-02-16 07:23:05 +0100

    real

---
+++ Mario & Luigi Remeiku/scripts/debug_arc_test.gd
@@ -0,0 +1,7 @@
+extends Sprite
+
+onready var enemy = $"../Sprite2"
+
+func _ready():
+	print(enemy)
+#	void draw_arc ( Vector2 center, float radius, float start_angle, float end_angle, int point_count, Color color, float width=1.0, bool antialiased=false )
